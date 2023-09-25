b0efeffb86e3722179e8d5807b765d8a6f241ea3
ASiracusa <fedorafilmz@gmail.com>
2023-09-25 02:09:39 -0400

    Added Features #8 and #9

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ app.get("/api/movieInfo", (req, res) => {
     makeQuery(res, sql, {});
 })
 
+app.get("/api/customerInfo", (req, res) => {
+    const sql = 'SELECT C.customer_id, C.first_name, C.last_name, C.email, C.active, C.store_id, C.create_date, A.address, Ci.city, Co.country '
+        + 'FROM sakila.customer AS C, sakila.address AS A, sakila.city AS Ci, sakila.country AS Co '
+        + 'WHERE C.customer_id=' + req.query.customerId + ' AND C.address_id=A.address_id AND A.city_id=Ci.city_id AND Ci.country_id=Co.country_id;';
+    makeQuery(res, sql, {});
+})
+
 app.get("/api/actorsTopMovies", (req, res) => {
     const sql = 'SELECT F.title, COUNT(*) AS rented '
         + 'FROM sakila.film AS F, sakila.rental AS R, sakila.inventory AS I, sakila.film_actor as FA '
@@ -52,6 +59,21 @@ app.get("/api/searchMovies", (req, res) => {
             + (hasAN ? (hasFN ? ' AND ' : '') + "F.film_id=FA.film_id AND FA.actor_id=A.actor_id AND CONCAT(A.first_name, ' ', A.last_name) LIKE '%" + req.query.actorName + "%'" : '')
             + (hasFG ? (hasFN || hasAN ? ' AND ' : '') + "F.film_id=FC.film_id AND FC.category_id=C.category_id AND C.name='" + req.query.filmGenre + "'" : '')
         + ' ORDER BY F.title ASC LIMIT 100;';
+    console.log(sql);
+    makeQuery(res, sql, {});
+})
+
+app.get("/api/searchCustomers", (req, res) => {
+    const hasCI = req.query.customerId.length > 0;
+    const hasFN = req.query.firstName.length > 0;
+    const hasLN = req.query.lastName.length > 0;
+    const sql = 'SELECT C.customer_id, C.first_name, C.last_name '
+        + 'FROM sakila.customer AS C'
+        + (hasCI || hasFN || hasLN ? ' WHERE ' : '')
+            + (hasCI ? "C.customer_id=" + req.query.customerId : '')
+            + (hasFN ? (hasCI ? ' AND ' : '') + "C.first_name LIKE '%" + req.query.firstName + "%'" : '')
+            + (hasLN ? (hasCI || hasFN ? ' AND ' : '') + "C.last_name LIKE '%" + req.query.lastName + "%'" : '')
+        + ' ORDER BY C.first_name ASC, C.last_name ASC LIMIT 100;';
     console.log(sql);
     makeQuery(res, sql, {});
 })
